0c014eae1a5c2551bd28cf8b2b437a05bbfae9c2
m4son <mrsima@gmail.com>
2022-06-26 18:48:16 +0200

    dont print team chats
    added game ending print and optional current score

--- aq2_log2discord.py
+++ aq2_log2discord.py
@@ -17,6 +17,7 @@ from discord_webhook import DiscordWebhook
 WEBHOOK = "WEBHOOK_URL" 
 DISCORD_USERNAME = "Console"
 CLOG = "logfile.log"
+CURRENT = False
 
 def follow(thefile):
     '''generator function that yields new lines in a file
@@ -34,9 +35,12 @@ event_msgs = {
         "join":"] A (.+) entered the game",
         "diss":"] A (.+)\[.+ disconnected",
         "cname":"] A (.+)\[.+ changed name to (.+)",
-        "chat":"] T (.+): (.+)",
+        "chat":"] T (\w.+)\: (.+)",
+        "dead":"] T \[DEAD] (\w.+)\: (.+)",
         "map":"] A Next map is (.+)",
         "map2":"] A SpawnServer: (.+)",
+        "score":"] A Current score is.+\:.(.+) to .+\: (.+)",
+        "gameend":"] A Game ending at: (.+)",
 }
 
 if __name__ == '__main__':
@@ -47,7 +51,14 @@ if __name__ == '__main__':
         for k, v in event_msgs.items():
             m = re.search(v,line)
             if m and 'MVDSPEC' not int m.group(1):
+                print(line.strip('\n'))
                 msg = line.strip('\n')[20:]
+                if k == "score":
+                    last_score = m
+                    if CURRENT:
+                        fmsg ='***``` Current score: {} to {}\n```***'.format(m.group(1),m.group(2))
+                if k == "gameend":
+                    fmsg ='***``` Game ends with score {} to {}\n```***'.format(last_score.group(1),last_score.group(2))
                 if k == "cname":
                     fmsg ='```diff\n- {} is known as {}\n```'.format(m.group(1),m.group(2))
                 if k == "diss":
@@ -56,8 +67,11 @@ if __name__ == '__main__':
                     fmsg ='```diff\n- {} entered the game\n```'.format(m.group(1))
                 if k == "chat":
                     fmsg ='```diff\n+ {}: {}\n```'.format(m.group(1),m.group(2))
+                if k == "dead":
+                    fmsg ='```diff\n+ [DEAD] {}: {}\n```'.format(m.group(1),m.group(2))
                 if k == "map" or k == "map2":
                     fmsg ='**```fix\n SpawnMap: {}\n```**'.format(m.group(1))
-                
-                webhook = DiscordWebhook(url=WEBHOOK, username=DISCORD_USERNAME, rate_limit_retry=True, content=fmsg )
-                response = webhook.execute()
+                if fmsg:
+                    webhook = DiscordWebhook(url=WEBHOOK, username=DISCORD_USERNAME, rate_limit_retry=True, content=fmsg )
+                    response = webhook.execute()
+                    del fmgs
